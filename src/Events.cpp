4cceae94c990a3dea6e7c225798c088172d5254a
Styyx <styyx228@gmail.com>
2023-07-03 10:52:09 +0200

    fixed crash with magic equipped

--- src/Events.cpp
+++ src/Events.cpp
@@ -1,5 +1,4 @@
 #include "Events.h"
-
 #include "Utility.h"
 #include "Settings.h"
 #include "Hooks.h"
@@ -15,19 +14,28 @@ namespace Events {
         if (!a_event) return RE::BSEventNotifyControl::kContinue;
         
         auto av_to_heal = RE::ActorValue::kMagicka;        
-        auto modifier = Settings::regeneration_value * 0.01;        
-        
+        auto modifier = Settings::regeneration_value * 0.01;  
+        const auto player = RE::PlayerCharacter::GetSingleton(); 
+
         if (a_event->cause) {
             if (a_event->cause->IsPlayerRef()) {
                 if (auto targ = a_event->target.get(); targ->As<RE::Actor>()) {
-                    if (const auto player = RE::PlayerCharacter::GetSingleton(); 
-                        const auto equipped_right = player->GetEquippedObject(false)) {
-                        if ( const auto weapon = equipped_right->As<RE::TESObjectWEAP>();
-                            weapon->IsOneHandedSword() || weapon->IsOneHandedAxe() || weapon->IsOneHandedMace()) {
-                            if (const auto magicka_pct = Hooks::GetActorValuePercent(player->As<RE::Actor>(), RE::ActorValue::kMagicka);
-                                const auto magicka_av =  player->AsActorValueOwner()->GetActorValue(RE::ActorValue::kMagicka)) { 
-                                if (magicka_pct <= Settings::trigger_value && player->IsInCombat()) {
-                                    player->AsActorValueOwner()->RestoreActorValue(RE::ACTOR_VALUE_MODIFIER::kDamage, av_to_heal, player->AsActorValueOwner()->GetBaseActorValue(RE::ActorValue::kMagicka) * modifier);
+                    if (!player->GetActorRuntimeData().selectedSpells[RE::Actor::SlotTypes::kRightHand]) {
+                        if (const auto equipped_right = player->GetEquippedObject(false)) {
+                            if (const auto weapon = equipped_right->As<RE::TESObjectWEAP>();
+                                weapon->IsOneHandedSword() || weapon->IsOneHandedAxe() || weapon->IsOneHandedMace()) {
+                                if (auto magicka_pct =
+                                        Hooks::GetActorValuePercent(player->As<RE::Actor>(), RE::ActorValue::kMagicka) *
+                                        100;
+                                    const auto magicka_av =
+                                        player->AsActorValueOwner()->GetActorValue(RE::ActorValue::kMagicka)) {
+                                    if (magicka_pct <= Settings::trigger_value && player->IsInCombat()) {
+                                        logger::info("Magicka Percentage is {}", magicka_pct);
+                                        player->AsActorValueOwner()->RestoreActorValue(
+                                            RE::ACTOR_VALUE_MODIFIER::kDamage, av_to_heal,
+                                            player->AsActorValueOwner()->GetBaseActorValue(RE::ActorValue::kMagicka) *
+                                                modifier);
+                                    }
                                 }
                             }
                         }                                                
