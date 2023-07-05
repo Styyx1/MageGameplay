6626a76d87f6b62d963be7a15e9630de54038bde
Styyx <styyx228@gmail.com>
2023-07-05 06:13:50 +0200

    fixed stamina regen not working

--- src/Events.cpp
+++ src/Events.cpp
@@ -28,38 +28,39 @@ namespace Events {
                         player->GetActorRuntimeData().selectedSpells[RE::Actor::SlotTypes::kLeftHand] && player->HasPerk(Utility::AbsorbPerk)) {
                         if (const auto equipped_right = player->GetEquippedObject(false)) {
                             if (const auto weapon = equipped_right->As<RE::TESObjectWEAP>();
-                                weapon->IsOneHandedSword() || weapon->IsOneHandedAxe() || weapon->IsOneHandedMace()) {
-                                if (a_event->source == player->GetActorRuntimeData()
-                                                           .selectedSpells[RE::Actor::SlotTypes::kLeftHand]
-                                                           ->GetFormID()) {
-                                    logger::info("noticed projectile");
-                                    if (const auto equipped_left = player->GetEquippedObject(true)) {
-                                        if (!equipped_left->IsWeapon() &&
-                                            player->GetActorRuntimeData()
-                                                .selectedSpells[RE::Actor::SlotTypes::kLeftHand]) {
-                                            logger::info("event ready for spell");
-                                            if (auto stam_pct = Hooks::GetActorValuePercent(player->As<RE::Actor>(),
-                                                                                            RE::ActorValue::kStamina) *
-                                                                100;
-                                                const auto stamina_av = player->AsActorValueOwner()->GetActorValue(
-                                                    RE::ActorValue::kStamina)) {
-                                                if (stam_pct <= Settings::trigger_value && player->IsInCombat()) {
-                                                    player->AsActorValueOwner()->RestoreActorValue(
-                                                        RE::ACTOR_VALUE_MODIFIER::kDamage, RE::ActorValue::kStamina,
-                                                        player->AsActorValueOwner()->GetBaseActorValue(
-                                                            RE::ActorValue::kStamina) *
-                                                            modifier);
-                                                    logger::info("healed stamina");
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                                weapon->IsOneHandedSword() || weapon->IsOneHandedAxe() || weapon->IsOneHandedMace()) {                                
                                 if (auto magicka_pct =
                                         Hooks::GetActorValuePercent(player->As<RE::Actor>(), RE::ActorValue::kMagicka) *
                                         100;
                                     const auto magicka_av =
                                         player->AsActorValueOwner()->GetActorValue(RE::ActorValue::kMagicka)) {
+                                    if (a_event->source == player->GetActorRuntimeData()
+                                                               .selectedSpells[RE::Actor::SlotTypes::kLeftHand]
+                                                               ->GetFormID()) {
+                                        logger::info("noticed projectile");
+                                        if (const auto equipped_left = player->GetEquippedObject(true)) {
+                                            if (!equipped_left->IsWeapon() &&
+                                                player->GetActorRuntimeData()
+                                                    .selectedSpells[RE::Actor::SlotTypes::kLeftHand]) {
+                                                logger::info("event ready for spell");
+                                                if (auto stam_pct =
+                                                        Hooks::GetActorValuePercent(player->As<RE::Actor>(),
+                                                                                    RE::ActorValue::kStamina) *
+                                                        100;
+                                                    const auto stamina_av = player->AsActorValueOwner()->GetActorValue(
+                                                        RE::ActorValue::kStamina)) {
+                                                    if (stam_pct <= Settings::trigger_value && player->IsInCombat()) {
+                                                        player->AsActorValueOwner()->RestoreActorValue(
+                                                            RE::ACTOR_VALUE_MODIFIER::kDamage, RE::ActorValue::kStamina,
+                                                            player->AsActorValueOwner()->GetBaseActorValue(
+                                                                RE::ActorValue::kStamina) *
+                                                                modifier);
+                                                        logger::info("healed stamina");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                     if (magicka_pct <= Settings::trigger_value && player->IsInCombat() && a_event->source == weapon->GetFormID()) {
                                         //logger::info("Magicka Percentage is {}", magicka_pct);
                                         player->AsActorValueOwner()->RestoreActorValue(
